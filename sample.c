00be69e45aa496d1b3cb3fdb76318dafdec4fb3f
varshitha157 <varshithabhairam7@gmail.com>
2024-08-10 14:43:04 +0530

    my first commit

---
+++ sample.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int A, B, sum = 0;
+
+    // Ask user to enter the two numbers
+    printf("Enter two integers: ");
+
+    // Read two numbers from the user || A = 2, B = 3
+    scanf("%d %d", &A, &B);
+
+    // Calculate the addition of A and B
+    // using '+' operator
+    sum = A + B;
+
+    // Print the sum
+    printf("Sum: %d", sum);
+
+    return 0;
+}
